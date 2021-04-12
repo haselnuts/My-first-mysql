c80f3a1b31ec335dc22893bb3060ea09436b3398
haselnuts <haselnuts@hotmail.com>
2021-04-12 10:46:33 +0000

    prepare chanllenge 4

--- test.sql
+++ test.sql
@@ -1,2 +1,6 @@
-select count(*) from Track;
-select count(*) from Artist;
+/* type of Grung, Track Name, Album.Name, Artist*/
+select Genre.Name, Track.name, Album.Name, Artist.Name from Track
+inner join Artist on Track.ArtistID = Artist.ArtistID
+inner join Album on Track.AlbumID = Album.AlbumID
+inner join Playlist on PlaylistTrack.PlaylistID = Playlist.PlaylistID
+inner join 
